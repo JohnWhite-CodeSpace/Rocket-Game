a9257d3568cf1216623ff7472612a1931b360721
Pablosnito <128155052+Pablosnito@users.noreply.github.com>
2023-06-14 02:32:58 +0200

    Update AssetSetter.java

--- src/Game/AssetSetter.java
+++ src/Game/AssetSetter.java
@@ -1,5 +1,6 @@
 package Game;
 
+import Object.OBJ_AlienSpaceship;
 import Object.OBJ_Asteroid;
 import Object.OBJ_AsteroidBelt;
 import Object.OBJ_Comet;
@@ -13,6 +14,8 @@ public class AssetSetter {
 	int[] m = new int[15];
 	double[] o = new double[350];
 	double[] p = new double[350];
+	int[] r = new int[20];
+	int[] s =new int[20];
 	int max = 15;
 	int min = -15;
 	int max2 = 8;
@@ -63,4 +66,18 @@ public class AssetSetter {
 	public void SetSol() {
 		gp.sol.set(gp.Tilesize*250,gp.Tilesize*250,"planet",true,null);
 	}
+	public void setAlienSpaceship() {
+		for(int i=0; i<r.length; i++) {
+			r[i] = (random2.nextInt(5)*(max-min)+min);
+			s[i] = (random2.nextInt(5)*(max2-min2)+min2);
+			gp.alienrocket[i] = new OBJ_AlienSpaceship(gp);
+			int worldx = gp.Tilesize*250+ gp.Tilesize*r[i]-gp.Tilesize*s[i]
+					+10*s[i]-5*r[i];
+			int worldy = gp.Tilesize*250+ gp.Tilesize*r[i]-gp.Tilesize*k[i]
+					+10*s[i]-5*r[i];
+			gp.alienrocket[i].set(worldx,worldy,"alienrocket",true,null);
+		}
+
+	}
 }
+
